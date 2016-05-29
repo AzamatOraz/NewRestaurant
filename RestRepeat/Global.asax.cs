fae2e5d7a694603e1505f457e7ce55062e3f35b3
azamat <azamatoraz@gmail.com>
2016-05-29 20:16:37 +0600

    added logging

--- RestRepeat/Global.asax.cs
+++ RestRepeat/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using RestRepeat.DAL;
+using System.Data.Entity.Infrastructure.Interception;
 
 namespace RestRepeat
 {
@@ -16,6 +18,8 @@ namespace RestRepeat
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DbInterception.Add(new RestaurantInterceptorTransientErrors());
+            DbInterception.Add(new RestaurantInterceptorLogging());
         }
     }
 }
