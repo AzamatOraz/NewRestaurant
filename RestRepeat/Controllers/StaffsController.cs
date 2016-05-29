a054e6c09b96a9aae50fbc36b07ef614b10213e8
azamat <azamatoraz@gmail.com>
2016-05-30 02:30:49 +0600

    Added design to staff create and index pages

--- RestRepeat/Controllers/StaffsController.cs
+++ RestRepeat/Controllers/StaffsController.cs
@@ -56,7 +56,7 @@ namespace RestRepeat.Controllers
                     break;
             }
 
-            int pageSize = 3;
+            int pageSize = 5;
             int pageNumber = (page ?? 1);
             return View(staffs.ToPagedList(pageNumber, pageSize));
         }
